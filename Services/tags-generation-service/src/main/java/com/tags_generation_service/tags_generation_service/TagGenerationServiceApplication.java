1c3004e449724e2876a60e6f267b527920e5a960
sujalsharmaa <sujalsharma151@gmail.com>
2025-11-03 19:17:08 +0530

    fixed pom.xml

---
+++ Services/tags-generation-service/src/main/java/com/tags_generation_service/tags_generation_service/TagGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tags_generation_service.tags_generation_service; 
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+@SpringBootApplication 
+public class TagGenerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagGenerationServiceApplication.class, args);
+	}
+
+}
