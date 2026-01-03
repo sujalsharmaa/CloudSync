dcc22d12223551c4f43ec2916a92b1f19038e8c2
sujalsharmaa <sujalsharma151@gmail.com>
2026-01-04 00:05:24 +0530

    fixed small bugs

---
+++ Services/search-service/src/main/java/com/search_service/search_service/config/OpenApiConfig.java
@@ -0,0 +1,59 @@
+package com.search_service.search_service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${info.app.name:Auth Service}")
+    private String appName;
+
+    @Value("${info.app.description:CloudSync Authentication Service}")
+    private String appDescription;
+
+    @Value("${info.app.version:1.0.0}")
+    private String appVersion;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(appName)
+                        .version(appVersion)
+                        .description(appDescription)
+                        .contact(new Contact()
+                                .name("CloudSync Team")
+                                .email("sujalsharma151@gmail.com")
+                                .url("https://drive.sujalsharma.in"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server()
+                                .url("https://auth.sujalsharma.in")
+                                .description("Production Server"),
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Local Development")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT token obtained from /login/oauth2/code/google")));
+    }
+}
